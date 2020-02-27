1c9755063a249a3ad2a3b94a0c99ee51156d8776
batuhangobekli <btubatuhan@gmail.com>
2020-02-27 13:03:02 +0300

    Chocolate app added.

---
+++ 1-Chocolate App/Chocotastic-starter/Chocotastic/ChocolatesOfTheWorldViewController.swift
@@ -0,0 +1,102 @@
+/// Copyright (c) 2019 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ChocolatesOfTheWorldViewController: UIViewController {
+  private let disposeBag = DisposeBag()
+
+  @IBOutlet private var cartButton: UIBarButtonItem!
+  @IBOutlet private var tableView: UITableView!
+  let europeanChocolates = Observable.just(Chocolate.ofEurope)
+}
+
+//MARK: View Lifecycle
+extension ChocolatesOfTheWorldViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupCartObserver()
+    setupCellConfiguration()
+    setupCellTapHandling()
+    title = "Chocolate!!!"
+  }
+}
+
+//MARK: - Rx Setup
+private extension ChocolatesOfTheWorldViewController {
+  func setupCartObserver() {
+    //1
+    ShoppingCart.sharedCart.chocolates.asObservable()
+      .subscribe(onNext: { //2
+        [unowned self] chocolates in
+        self.cartButton.title = "\(chocolates.count) \u{1f36b}"
+      })
+      .disposed(by: disposeBag) //3
+  }
+  func setupCellConfiguration() {
+    //1
+    europeanChocolates
+      .bind(to: tableView
+        .rx //2
+        .items(cellIdentifier: ChocolateCell.Identifier,
+               cellType: ChocolateCell.self)) { //3
+                row, chocolate, cell in
+                cell.configureWithChocolate(chocolate: chocolate) //4
+      }
+      .disposed(by: disposeBag) //5
+  }
+  func setupCellTapHandling() {
+    tableView
+      .rx
+      .modelSelected(Chocolate.self) //1
+      .subscribe(onNext: { [unowned self] chocolate in // 2
+        let newValue =  ShoppingCart.sharedCart.chocolates.value + [chocolate]
+        ShoppingCart.sharedCart.chocolates.accept(newValue) //3
+        
+        if let selectedRowIndexPath = self.tableView.indexPathForSelectedRow {
+          self.tableView.deselectRow(at: selectedRowIndexPath, animated: true)
+        } //4
+      })
+      .disposed(by: disposeBag) //5
+  }
+}
+
+//MARK: - Imperative methods
+private extension ChocolatesOfTheWorldViewController {
+ 
+}
+
+
+// MARK: - SegueHandler
+extension ChocolatesOfTheWorldViewController: SegueHandler {
+  enum SegueIdentifier: String {
+    case goToCart
+  }
+}

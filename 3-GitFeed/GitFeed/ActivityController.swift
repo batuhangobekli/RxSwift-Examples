bd615a43685f58f50e5f165c59d93533909c1763
Batuhan Göbekli <btubatuhan@gmail.com>
2021-01-19 15:14:33 +0300

    Api implemented.

--- 3-GitFeed/GitFeed/ActivityController.swift
+++ 3-GitFeed/GitFeed/ActivityController.swift
@@ -27,55 +27,82 @@ import Kingfisher
 
 
 class ActivityController: UITableViewController {
-
-  let repo = "ReactiveX/RxSwift"
-
-  fileprivate let events = BehaviorRelay<[Event]>(value: [])
-  fileprivate let bag = DisposeBag()
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    title = repo
-
-    self.refreshControl = UIRefreshControl()
-    let refreshControl = self.refreshControl!
-
-    refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
-    refreshControl.tintColor = UIColor.darkGray
-    refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
-    refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
-
-    refresh()
-  }
-
-    @objc func refresh() {
-    fetchEvents(repo: repo)
-  }
-
-  func fetchEvents(repo: String) {
-    let response = Observable.from([repo])
     
-    response.map { urlString -> URL in
-        return URL(string: "https://api.github.com/repos/\(urlString)/events")!
-    }.map { url -> URLRequest in
-        return URLRequest(url: url)
-    }.flatMap { request -> Observable<(response:HTTPURLResponse,data:Data)> in
-        return URLSession.shared.rx.response(request: request)
-    }.share(replay: 1)
-  }
-
-  // MARK: - Table Data Source
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return events.value.count
-  }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let event = events.value[indexPath.row]
-
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
-    cell.textLabel?.text = event.name
-    cell.detailTextLabel?.text = event.repo + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
-    cell.imageView?.kf.setImage(with: event.imageUrl, placeholder: UIImage(named: "blank-avatar"))
-    return cell
-  }
+    let repo = "ReactiveX/RxSwift"
+    
+    fileprivate let events = BehaviorRelay<[Event]>(value: [])
+    fileprivate let bag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = repo
+        
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        
+        refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+        refreshControl.tintColor = UIColor.darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        refresh()
+    }
+    
+    @objc func refresh() {
+        fetchEvents(repo: repo)
+    }
+    
+    func fetchEvents(repo: String) {
+        let response = Observable.from([repo])
+        
+        response.map { urlString -> URL in
+            return URL(string: "https://api.github.com/repos/\(urlString)/events")!
+        }.map { url -> URLRequest in
+            return URLRequest(url: url)
+        }.flatMap { request -> Observable<(response:HTTPURLResponse,data:Data)> in
+            return URLSession.shared.rx.response(request: request)
+        }.share(replay: 1)
+        .filter { response, _ in
+            return 200..<300 ~= response.statusCode
+        }.map { _, data -> [[String: Any]] in
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+                  let result = jsonObject as? [[String: Any]] else {
+                return []
+            }
+            return result
+        }.filter { objects in
+            return objects.count > 0
+        }.map { objects in
+            return objects.map(Event.init)
+        }.subscribe(onNext:{[weak self] newEvents in
+            self?.processEvents(newEvents)
+        }).disposed(by:bag)
+    }
+    
+    func processEvents(_ newEvents: [Event]) {
+        var updatedEvents = newEvents + events.value
+        if updatedEvents.count > 50 {
+        updatedEvents = Array<Event>(updatedEvents.prefix(upTo: 50))
+        }
+        events.accept(updatedEvents)
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.refreshControl?.endRefreshing()
+        }
+    }
+    
+    // MARK: - Table Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.value.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let event = events.value[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = event.name
+        cell.detailTextLabel?.text = event.repo + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
+        cell.imageView?.kf.setImage(with: event.imageUrl, placeholder: UIImage(named: "blank-avatar"))
+        return cell
+    }
 }
